fee525f1bdc22d4becf2965321d171ddec8c1fe5
Peerarust Siriamphan <genoyoyo@gmail.com>
2018-07-14 13:32:01 +0700

    Add DHT 22

--- lotto/lotto.ino
+++ lotto/lotto.ino
@@ -17,10 +17,10 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 //Assign Sensor
-DeviceAddress tempHand1 = { 0x28, 0xFF, 0xEE, 0xBA, 0x84, 0x16, 0x05, 0x3F };
-DeviceAddress tempHand2 = { 0x28, 0xFF, 0x1A, 0x46, 0x84, 0x16, 0x03, 0x38 };
-DeviceAddress tempHand3 = { 0x28, 0xFF, 0xE8, 0x3F, 0x85, 0x16, 0x05, 0x50 };
-DeviceAddress tempHand4 = { 0x28, 0xFF, 0xBE, 0x48, 0xB3, 0x16, 0x05, 0x78 };
+DeviceAddress sensorHand1 = { 0x28, 0xFF, 0xEE, 0xBA, 0x84, 0x16, 0x05, 0x3F };
+DeviceAddress sensorHand2 = { 0x28, 0xFF, 0x1A, 0x46, 0x84, 0x16, 0x03, 0x38 };
+DeviceAddress sensorHand3 = { 0x28, 0xFF, 0xE8, 0x3F, 0x85, 0x16, 0x05, 0x50 };
+DeviceAddress sensorHand4 = { 0x28, 0xFF, 0xBE, 0x48, 0xB3, 0x16, 0x05, 0x78 };
 
 float Temp1;
 
@@ -36,10 +36,10 @@ void setup() {
 
   sensors.begin();
 
-  sensors.setResolution(tempHand1, 12);
-  sensors.setResolution(tempHand2, 12);
-  sensors.setResolution(tempHand3, 12);
-  sensors.setResolution(tempHand4, 12);
+  sensors.setResolution(sensorHand1, 12);
+  sensors.setResolution(sensorHand2, 12);
+  sensors.setResolution(sensorHand3, 12);
+  sensors.setResolution(sensorHand4, 12);
 
 
 
@@ -54,7 +54,19 @@ void loop() {
   //Read Sensor
   sensors.requestTemperatures();
   delay(1000);
-
+  
+  Serial.print("\n");
+  Serial.print(getTemperature(sensorHand1));
+  Serial.print("\n");
+  Serial.print(getTemperature(sensorHand2));
+  Serial.print("\n");
+  Serial.print(getTemperature(sensorHand3));
+  Serial.print("\n");
+  Serial.print(getTemperature(sensorHand4));
+  Serial.print("\n\n\n");
+  Serial.print(dht1.readTemperature());
+  Serial.print("\n");
+  Serial.print(dht1.readHumidity());
 
 
 }
