34cf4857160df8386fd4e69645469093f18ee100
Peerarust Siriamphan <genoyoyo@gmail.com>
2018-07-14 20:42:53 +0700

    Add button

--- lotto/ambiance/ambiance.ino
+++ lotto/ambiance/ambiance.ino
@@ -4,45 +4,57 @@
 // Module connection pins (Digital Pins)
 #define CLK 2
 #define DIO 3
+#define Button 12
+#define Light 6
+#define LightSource 5
 
 // The amount of time (in milliseconds) between tests
 #define TEST_DELAY   2000
 
 TM1637Display display(CLK, DIO);
 
+int buttonState;
+
 void setup()
 {
+  pinMode(Button, INPUT);
+  pinMode(Light, OUTPUT);
+  pinMode(LightSource, OUTPUT);
+  digitalWrite(Light, LOW);
+  digitalWrite(LightSource, HIGH);
+
+  Serial.begin(9600); // the bigger number the better
+
 }
 
 void loop()
 {
+  digitalWrite(Light, LOW);
   int k;
   int x;
   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
   display.setBrightness(0x0f);
 
-  // All segments on
-  display.setSegments(data);
-  delay(TEST_DELAY);
+
+  buttonState = digitalRead(Button);
+
+  Serial.println(buttonState);
+
+  if (buttonState == 1) {
+
+    digitalWrite(Light, HIGH);
 
 
+    for (x = 10; x >= 0 ; x--) {
 
-  // Selectively set different digits
-  data[0] = 0b01001001;
-  data[1] = display.encodeDigit(1);
-  data[2] = display.encodeDigit(2);
-  data[3] = display.encodeDigit(3);
+      display.showNumberDec(x, true);
+      delay(1000);
+    }
 
-  display.showNumberDec(120, true);
-
-  for (x = 10; x >= 0 ; x--) {
-
-    display.showNumberDec(x, true);
-    delay(1000);
+  } else {
+    digitalWrite(Light, LOW);
   }
 
-  while (1);
-
-
+  digitalWrite(Light, LOW);
 
 }
