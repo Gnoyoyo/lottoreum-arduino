d727046a7755eb46fdb0978db1621021df23d73f
Peerarust Siriamphan <genoyoyo@gmail.com>
2018-07-14 17:38:33 +0700

    Delete unused code

--- lotto/ambiance/ambiance.ino
+++ lotto/ambiance/ambiance.ino
@@ -8,13 +8,6 @@
 // The amount of time (in milliseconds) between tests
 #define TEST_DELAY   2000
 
-const uint8_t SEG_DONE[] = {
-  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
-  SEG_C | SEG_E | SEG_G,                           // n
-  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-};
-
 TM1637Display display(CLK, DIO);
 
 void setup()
